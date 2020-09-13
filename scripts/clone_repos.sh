2a32b4eb0808ba614b3d4d7936bd9baec5f4d2bb
Simonbelete <simonbelete@gmail.com>
2020-09-13 08:00:18 +0300

    simple clone all guya-ltds repo command

---
+++ scripts/clone_repos.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+# Base
+git clone https://github.com/Guya-LTD/guya.git
+
+# Bits
+git clone https://github.com/Guya-LTD/bits.git
+
+# GxDriver
+git clone https://github.com/Guya-LTD/gxdriver.git
+
+# User Service
+git clone https://github.com/Guya-LTD/user.git
+
+# Software requirement specfication
+git clone https://github.com/Guya-LTD/srs.git
+
+# Gatekeeper Service
+git clone https://github.com/Guya-LTD/gatekeeper.git
+
+# Catalog Service
+git clone https://github.com/Guya-LTD/catalog.git
+
+# Dymo Label Maker
+git clone https://github.com/Guya-LTD/dymo.git
+
+# Guya's Cascading Style Sheets
+git clone https://github.com/Guya-LTD/gcss.git
+
+# Xpress Service
+git clone https://github.com/Guya-LTD/xpress.git
+
+# Branch Service
+git clone https://github.com/Guya-LTD/branch.git
+
+# Chipmunk SMS and Email marketing platform for bulk
+git clone https://github.com/Guya-LTD/chipmunk.git
+
+# Postgres
+git clone https://github.com/Guya-LTD/postgres.git
+
+# Shop-web Ui
+git clone https://github.com/Guya-LTD/shop-web.git
+
+# Xpress-web Ui
+git clone https://github.com/Guya-LTD/xpress-web.git
+
+# Nginx
+git clone https://github.com/Guya-LTD/nginx.git
+
+# Redis
+git clone https://github.com/Guya-LTD/redis.git
+
+# Reference service
+git clone https://github.com/Guya-LTD/refme.git
+
+# Xtrack Service
+git clone https://github.com/Guya-LTD/xtrack.git
+
+# Admin-panel
+git clone https://github.com/Guya-LTD/admin-panel.git
+
+# Alfa-geez-node
+git clone https://github.com/Guya-LTD/alfa-geez-node.git
+
+# Chat Service
+git clone https://github.com/Guya-LTD/chat.git
+
+# Cart Service
+git clone https://github.com/Guya-LTD/cart.git
+
+# Pyrat
+git clone https://github.com/Guya-LTD/pyrat.git
+
+# Payment
+git clone https://github.com/Guya-LTD/payment.git
+
+# Storybook
+git clone https://github.com/Guya-LTD/storybook.git
+
+# Python-logstash
+git clone https://github.com/Guya-LTD/python-logstash.git
