66bbad1437e01b32933c1c26ef5f5af6e0449417
Simonbelete <simonbelete@gmail.com>
2020-09-30 15:21:21 +0300

    feat(docker) adding docker pull images

---
+++ docker-pulls.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker pull docker.elastic.co/kibana/kibana:7.9.1
+docker pull docker.elastic.co/elasticsearch/elasticsearch:7.9.1
+docker pull docker.elastic.co/logstash/logstash:7.9.1
+docker pull docker.io/bitnami/rabbitmq:3.8.9-debian-10-r0
+docker pull docker.io/bitnami/postgresql:11.9.0-debian-10-r34
+docker pull docker.io/bitnami/redis:6.0.8-debian-10-r0
+docker pull docker.io/bitnami/mongodb:4.4.1-debian-10-r13
