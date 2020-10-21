e5bbec31f78ba46497e31ab9d35ef4f1d4890885
Simonbelete <simonbelete@gmail.com>
2020-10-21 05:00:21 +0300

    feat(docker) adding mysql to docker pull

--- docker-pulls.sh
+++ docker-pulls.sh
@@ -7,3 +7,4 @@ docker pull docker.io/bitnami/rabbitmq:3.8.9-debian-10-r0
 docker pull docker.io/bitnami/postgresql:11.9.0-debian-10-r34
 docker pull docker.io/bitnami/redis:6.0.8-debian-10-r0
 docker pull docker.io/bitnami/mongodb:4.4.1-debian-10-r13
+docker pull docker.io/bitnami/mysql:8.0.21-debian-10-r46
